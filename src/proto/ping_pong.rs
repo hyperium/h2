701c14badb2c1eff03c6cd29f09bd67070d27e27
Carl Lerche <me@carllerche.com>
2017-06-27 12:49:40 -0700

    comments

--- src/proto/ping_pong.rs
+++ src/proto/ping_pong.rs
@@ -28,7 +28,12 @@ impl<T> Stream for PingPong<T>
     type Error = ConnectionError;
 
     fn poll(&mut self) -> Poll<Option<Frame>, ConnectionError> {
-        self.inner.poll()
+        // returns from socek to application
+        self.inner.poll() // inner frame?
+            // match on ping without ack.
+            // send pong on inner:
+            // self.inner.start_send(Pong) ....
+            // what happens when sending pong isn't ready?
     }
 }
 
@@ -40,11 +45,14 @@ impl<T> Sink for PingPong<T>
     type SinkError = ConnectionError;
 
     fn start_send(&mut self, item: Frame) -> StartSend<Frame, ConnectionError> {
+        // ensure pending pong is complete before sending `item`
+        // client sends us frames here
         self.inner.start_send(item)
     }
 
     fn poll_complete(&mut self) -> Poll<(), ConnectionError> {
         self.inner.poll_complete()
+            // if pong hasn't sent, poll it before polling inner
     }
 }
 
