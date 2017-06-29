2f5a811082df880c78c00823608ec7052e8dbc7f
Oliver Gould <ver@buoyant.io>
2017-06-29 19:03:47 +0000

    test ping pong and passthrough

--- src/proto/ping_pong.rs
+++ src/proto/ping_pong.rs
@@ -58,6 +58,8 @@ impl<T> Stream for PingPong<T>
                     // determine how to proceed.
                     continue;
                 }
+
+                // Anything other than ping gets passed through.
                 poll => return poll,
             }
         }
@@ -87,8 +89,12 @@ impl<T> Sink for PingPong<T>
     fn poll_complete(&mut self) -> Poll<(), ConnectionError> {
         // Try to flush the underlying sink.
         let poll = self.inner.poll_complete()?;
+        if self.pending_pongs.is_empty() {
+            return Ok(poll);
+        }
 
-        // Then, try to flush pending pongs.
+        // Then, try to flush pending pongs. Even if poll is not ready, we may be able to
+        // start sending pongs.
         while let Some(pong) = self.pending_pongs.pop_front() {
             if let AsyncSink::NotReady(pong) = self.inner.start_send(pong)? {
                 // If we can't flush all of the pongs, we're not ready. Save the pong to
@@ -98,8 +104,8 @@ impl<T> Sink for PingPong<T>
             }
         }
 
-        // Use the underlying sink's status if pongs are flushed.
-        Ok(poll)
+        // Because we've invoked start_send, poll_complete needs to be invoked again.
+        self.inner.poll_complete()
     }
 }
 
@@ -113,5 +119,130 @@ impl<T> ReadySink for PingPong<T>
             return Ok(Async::NotReady);
         }
         self.inner.poll_ready()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use bytes::Bytes;
+    use std::cell::RefCell;
+    use std::rc::Rc;
+
+    #[test]
+    fn responds_to_ping_with_pong() {
+        let trans = Transport::default();
+        let mut ping_pong = PingPong::new(trans.clone());
+
+        {
+            let mut trans = trans.0.borrow_mut();
+            let ping = Ping::ping(Bytes::from_static(b"buoyant!"));
+            trans.from_socket.push_back(ping.into());
+        }
+
+        match ping_pong.poll() {
+            Ok(Async::NotReady) => {} // cool
+            rsp => panic!("unexpected poll result: {:?}", rsp),
+        }
+
+        assert!(ping_pong.poll_complete().is_ok());
+        {
+            let mut trans = trans.0.borrow_mut();
+            assert_eq!(trans.to_socket.len(), 1);
+            match trans.to_socket.pop_front().unwrap() {
+                Frame::Ping(pong) => {
+                    assert!(pong.is_ack());
+                    assert_eq!(pong.into_payload(), Bytes::from_static(b"buoyant!"));
+                }
+                f => panic!("unexpected frame: {:?}", f),
+            }
+        }
+    }
+
+    #[test]
+    fn pong_passes_through() {
+        let trans = Transport::default();
+        let mut ping_pong = PingPong::new(trans.clone());
+
+        {
+            let mut trans = trans.0.borrow_mut();
+            let pong = Ping::pong(Bytes::from_static(b"buoyant!"));
+            trans.from_socket.push_back(pong.into());
+        }
+
+        match ping_pong.poll().unwrap() {
+            Async::Ready(Some(Frame::Ping(pong))) => {
+                assert!(pong.is_ack());
+                assert_eq!(pong.into_payload(), Bytes::from_static(b"buoyant!"));
+            }
+            f => panic!("unexpected frame: {:?}", f),
+        }
+
+        assert!(ping_pong.poll_complete().is_ok());
+        {
+            let trans = trans.0.borrow();
+            assert_eq!(trans.to_socket.len(), 0);
+        }
+    }
+
+    /// A stubbed transport for tests.a
+    ///
+    /// We probably want/have something generic for this?
+    #[derive(Clone, Default)]
+    struct Transport(Rc<RefCell<Inner>>);
+
+    #[derive(Default)]
+    struct Inner {
+        from_socket: VecDeque<Frame>,
+        to_socket: VecDeque<Frame>,
+        read_blocked: bool,
+        send_start_blocked: bool,
+        closing: bool,
+    }
+
+    impl Stream for Transport {
+        type Item = Frame;
+        type Error = ConnectionError;
+
+        fn poll(&mut self) -> Poll<Option<Frame>, ConnectionError> {
+            let mut trans = self.0.borrow_mut();
+            if trans.read_blocked || (!trans.closing && trans.from_socket.is_empty()) {
+                Ok(Async::NotReady)
+            } else {
+                Ok(trans.from_socket.pop_front().into())
+            }
+        }
+    }
+
+    impl Sink for Transport {
+        type SinkItem = Frame;
+        type SinkError = ConnectionError;
+
+        fn start_send(&mut self, item: Frame) -> StartSend<Frame, ConnectionError> {
+            let mut trans = self.0.borrow_mut();
+            if trans.closing || trans.send_start_blocked {
+                Ok(AsyncSink::NotReady(item))
+            } else {
+                trans.to_socket.push_back(item);
+                Ok(AsyncSink::Ready)
+            }
+        }
+
+        fn poll_complete(&mut self) -> Poll<(), ConnectionError> {
+            let trans = self.0.borrow();
+            if !trans.to_socket.is_empty() {
+                Ok(Async::NotReady)
+            } else {
+                Ok(Async::Ready(()))
+            }
+        }
+
+        fn close(&mut self) -> Poll<(), ConnectionError> {
+            {
+                let mut trans = self.0.borrow_mut();
+                trans.closing = true;
+            }
+            self.poll_complete()
+        }
     }
 }
