76712d2967686e29784dc4f22f161c7e18c813e3
Eliza Weisman <eliza@buoyant.io>
2017-09-07 10:35:48 -0500

    maybe this will work

---
+++ .ci/coverage.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+shopt -s extglob
+
+wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz &&
+tar xzf master.tar.gz &&
+cd kcov-master &&
+mkdir build &&
+cd build &&
+cmake .. &&
+make &&
+sudo make install &&
+cd ../.. &&
+rm -rf kcov-master
+
+for file in target/debug/!(*.*); do
+    [ -d "$file" ] && continue;
+    mkdir -p "target/cov/$(basename $file)";
+    kcov --exclude-pattern=/.cargo,/usr/lib "target/cov/$(basename $file)" "$file";
+done
+
+bash <(curl -s https://codecov.io/bash) &&
+echo "Uploaded code coverage"
