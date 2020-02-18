12d3a9f4e626d3260ef9110687506738b7fb73dd
ci <ci>
2020-02-18 00:57:17 +0000

    Deploy H2 API documentation

---
+++ implementors/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["h2"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;<a class=\"struct\" href=\"h2/frame/struct.StreamId.html\" title=\"struct h2::frame::StreamId\">StreamId</a>&gt; for <a class=\"struct\" href=\"h2/frame/struct.StreamId.html\" title=\"struct h2::frame::StreamId\">StreamId</a>","synthetic":false,"types":["h2::frame::stream_id::StreamId"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
