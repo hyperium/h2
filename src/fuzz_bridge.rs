6124dce75a7f33c1d806c84faeddc1c2273b40e3
davkor <david@adalogics.com>
2021-04-15 20:56:36 +0100

    more cleanup.
    Signed-off-by: davkor <david@adalogics.com>

--- src/fuzz_bridge.rs
+++ src/fuzz_bridge.rs
@@ -1,23 +1,23 @@
-use crate::hpack;
-use std::io::Cursor;
-//use bytes::{Buf, Bytes, BytesMut};
-use bytes::{Buf, BufMut, BytesMut};
-use http::header::{HeaderName, HeaderValue};
-
+#[cfg(fuzzing)]
 pub mod fuzz_logic {
-    use super::*;
+    #[cfg_attr(feature = "unstable", allow(missing_docs))]
+    use crate::hpack;
+    use std::io::Cursor;
+    use bytes::{BufMut, BytesMut};
+    use http::header::HeaderName;
+    //use super::*;
 
     pub fn fuzz_hpack(data_: &[u8]) {
         let mut decoder_ = hpack::Decoder::new(0);
         let mut buf = BytesMut::new();
         buf.extend(data_);
-        decoder_.decode(&mut Cursor::new(&mut buf), |h| {});
+        let _dec_res = decoder_.decode(&mut Cursor::new(&mut buf), |_h| {});
 
         if let Ok(s) = std::str::from_utf8(data_) {
             if let Ok(h) = http::Method::from_bytes(s.as_bytes()) {
                 let m_ = hpack::Header::Method(h);
                 let mut encoder = hpack::Encoder::new(0, 0);
-                let res_ = encode(&mut encoder, vec![m_]);
+                let _res = encode(&mut encoder, vec![m_]);
             }
         }
     }
@@ -26,19 +26,5 @@ pub mod fuzz_logic {
         let mut dst = BytesMut::with_capacity(1024);
         e.encode(None, &mut hdrs.into_iter(), &mut (&mut dst).limit(1024));
         dst
-    }
-
-    fn method(s: &str) -> hpack::Header<Option<HeaderName>> {
-        hpack::Header::Method(http::Method::from_bytes(s.as_bytes()).unwrap())
-    }
-
-    fn header(name: &str, val: &str) -> hpack::Header<Option<HeaderName>> {
-        let name = HeaderName::from_bytes(name.as_bytes()).unwrap();
-        let value = HeaderValue::from_bytes(val.as_bytes()).unwrap();
-
-        hpack::Header::Field {
-            name: Some(name),
-            value,
-        }
     }
 }
