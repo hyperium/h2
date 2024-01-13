c75575256e7cfabbbebe12023281b491dd60b351
Yusuke Tanaka <yusuktan@maguro.dev>
2024-01-13 18:49:46 +0900

    fix: set MAX_CONCURRENT_STREAMS to usize::MAX if no value is advertised initially

--- src/proto/streams/counts.rs
+++ src/proto/streams/counts.rs
@@ -147,9 +147,11 @@ impl Counts {
         self.num_remote_reset_streams -= 1;
     }
 
-    pub fn apply_remote_settings(&mut self, settings: &frame::Settings) {
-        if let Some(val) = settings.max_concurrent_streams() {
-            self.max_send_streams = val as usize;
+    pub fn apply_remote_settings(&mut self, settings: &frame::Settings, is_initial: bool) {
+        match settings.max_concurrent_streams() {
+            Some(val) => self.max_send_streams = val as usize,
+            None if is_initial => self.max_send_streams = usize::MAX,
+            None => {}
         }
     }
 
