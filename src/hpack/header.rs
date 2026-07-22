687787c2fbd5bab59e7f211c1794ff86df15cced
0x676e67 <gngppz@gmail.com>
2026-07-23 03:01:23 +0800

    Revert "perf(header): optimize `HeaderValue` decoding via zero-copy sharing (…"
    This reverts commit 55a0d9de0e977c0e4983efefbcf5a1f753ca9846.

--- src/hpack/header.rs
+++ src/hpack/header.rs
@@ -96,7 +96,7 @@ impl Header {
         } else {
             // HTTP/2 requires lower case header names
             let name = HeaderName::from_lowercase(&name)?;
-            let value = HeaderValue::from_maybe_shared(value)?;
+            let value = HeaderValue::from_bytes(&value)?;
 
             Ok(Header::Field { name, value })
         }
@@ -231,7 +231,7 @@ impl<'a> Name<'a> {
         match self {
             Name::Field(name) => Ok(Header::Field {
                 name: name.clone(),
-                value: HeaderValue::from_maybe_shared(value)?,
+                value: HeaderValue::from_bytes(&value)?,
             }),
             Name::Authority => Ok(Header::Authority(BytesStr::try_from(value)?)),
             Name::Method => Ok(Header::Method(Method::from_bytes(&value)?)),
